ac7c2732c35bbaa82f3d399cbfef77a2e0937e77
harha <toni.s.h@netikka.fi>
2016-11-07 21:56:34 +0200

    Initial commit.

---
+++ app/app.js
@@ -0,0 +1,12 @@
+// Angular instance
+var angular = require('angular');
+
+// App instance
+var app = angular.module('app', [
+	// 3rd party includes
+	require('angular-chart.js')
+]);
+
+// app includes
+require('./controller');
+
---
+++ app/controller/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// App instance
+var app = require('angular').module('app');
+
+// App controllers
+app.controller('OPCUA_Server_Ctrl', require('./opcua_server'));
